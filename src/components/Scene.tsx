58c2599d0060aa649661c2572f7ba8b014be0a47
Lumethra <maximilian.zhu@loewenrot.de>
2026-08-16 15:33:54 +0200

    working shuffle

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -11,41 +11,49 @@ interface IBoxProps {
     scale: number;
 }
 
-function Box({ position, scale }: IBoxProps) {
-    const mesh = useRef<any>(null)
-
-    useFrame((_, delta) => {
-        if (!mesh.current) return
-
-        const distance = position - mesh.current.position.x
-
-        mesh.current.position.x += distance * delta * 5
-    })
-    return (
-        <group>
-            <mesh
-                ref={mesh}
-                position={[0, scale / 2, 0]}
-                scale={[1, scale, 1]}
-            >
-                <boxGeometry />
-                <meshStandardMaterial />
-            </mesh>
-        </group>
-    )
-}
 
 export default function Scene() {
-    const [boxPos, setBoxPos] = useState(4.4)
+    const [numbers, setNumbers] = useState([2, 6, 3, 4, 5, 1, 7])
 
-    const move = () => {
-        setBoxPos((boxPos + 1.1) % 5.5)
+    const sort = () => {
+        setNumbers([1, 2, 3, 4, 5, 6]);
+    }
+
+    const shuffle = () => {
+        setNumbers(numbers.map(() => Math.floor((Math.random() * 7 + 1) * 100) / 100));
+
+        // console.log(Math.floor((Math.random() * 8 + 1) * 100) / 100)
+    }
+
+    function Box({ position, scale }: IBoxProps) {
+        const mesh = useRef<any>(null)
+
+        useFrame((_, delta) => {
+            if (!mesh.current) return
+
+            const distance = position - mesh.current.position.x
+
+            mesh.current.position.x += distance * delta * 5
+        })
+        return (
+            <group>
+                <mesh
+                    ref={mesh}
+                    position={[0, scale / 2, 0]}
+                    scale={[1, scale, 1]}
+                >
+                    <boxGeometry />
+                    <meshStandardMaterial />
+                </mesh>
+            </group>
+        )
     }
 
     return (
         <div style={{ width: "100vw", height: "100vh" }}>
             <div style={{ position: "absolute", top: 20, left: 20, zIndex: 10, display: "flex", gap: "10px" }}>
-                <button onClick={move} style={{ padding: "10px", cursor: "pointer" }}>Move</button>
+                <button onClick={sort} style={{ padding: "10px", cursor: "pointer" }}>Sort</button>
+                <button onClick={shuffle} style={{ padding: "10px", cursor: "pointer" }}>Shuffle</button>
             </div>
 
             <Canvas
@@ -55,12 +63,12 @@ export default function Scene() {
             >
                 <ambientLight intensity={0.5} />
                 <directionalLight position={[10, 10, 5]} intensity={1} castShadow />
-                <group position={[-2.2, -2.5, 0]}>
-                    <Box position={0} scale={1} />
-                    <Box position={1.1} scale={2} />
-                    <Box position={2.2} scale={3} />
-                    <Box position={3.3} scale={4} />
-                    <Box position={boxPos} scale={5} />
+                <group position={[-(numbers.length / 2 - 0.27), -(numbers.length / 2), 0]}>
+                    {numbers.map((value, index) => {
+                        return (
+                            <Box key={index} position={(index * 1.1)} scale={value} />
+                        )
+                    })}
                 </group>
             </Canvas>
         </div>
